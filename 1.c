02b5e10a177a173767e060568485afabf288a5f4
Omkar <omkarjamanale@gmail.com>
2024-03-01 16:34:07 +0530

    program is added

--- 1.c
+++ 1.c
@@ -7,3 +7,98 @@ particular day (A dynamically allocated String).
 b) Write functions create(), read() and display(); to create the calendar, to read the data
 from the keyboard and to print weeks activity details report on screen. 
   */
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define Days 7
+
+typedef struct 
+{
+    char *day;
+    int date;
+    char *activity;
+} Activity;
+
+Activity* createCal();
+void readCal(Activity *);
+void displayCal(Activity *);
+void freeCal(Activity*);
+
+int main()
+{
+    Activity *weekActivity = createCal();
+    readCal(weekActivity);
+    displayCal(weekActivity);
+    freeCal(weekActivity);
+    return 0;
+}
+
+Activity* createCal()
+{   
+    Activity *cal = (Activity *)calloc(Days, sizeof(Activity));
+    if (cal == NULL)
+    {
+        // Handle memory allocation failure
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < Days; i++)
+    {
+        cal[i].day = (char *)calloc(10, sizeof(char));
+        cal[i].activity = (char *)calloc(25, sizeof(char));
+        if (cal[i].day == NULL || cal[i].activity == NULL)
+        {
+            // Handle memory allocation failure
+            exit(EXIT_FAILURE);
+        }
+    }
+    return cal;
+}
+
+void readCal(Activity* weekActivity)
+{ 
+    for(int i = 0; i < Days; i++)
+    { 
+        int x = 0;
+        printf("\n Enter for Day-%d\n", i);
+        
+        printf("\nEnter the date: ");
+        scanf("%d", &weekActivity[i].date);
+
+        printf("Enter the day name: ");
+        scanf("%9s", weekActivity[i].day);
+        
+        printf("\nEnter (1) if there is any activity else (0): ");
+        scanf("%d", &x);
+        
+        
+        if (x == 1)
+        {
+            printf("\nEnter the Activity(in one word): ");
+            scanf(" %24s", weekActivity[i].activity);
+        }
+        printf("\n====================================\n");
+    }
+}
+
+void displayCal(Activity* Ac)
+{
+    printf("The Activity list of the week is: \n");
+    printf("sl no.\tDate\tDay\tActivity\n");
+
+    for(int i = 0; i < Days; i++)
+    {
+        printf("%d\t%d\t%s\t\t%s\n", i, Ac[i].date, Ac[i].day, Ac[i].activity);
+    }
+}
+
+void freeCal(Activity* Ac)
+{
+    for (int i = 0; i < Days; i++)
+    {
+        free(Ac[i].day);
+        free(Ac[i].activity);
+    }
+    free(Ac);
+}
